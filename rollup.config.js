69ce3627e7566b4c0ffe040587da9d67599bb3d7
Daniel Sternlicht <gdblogst@gmail.com>
2021-12-24 12:14:37 +0200

    Treeshakable (!!!), revealing only external components, types, and services

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,45 @@
+// Setup is based on the following article:
+// https://www.codefeetime.com/post/tree-shaking-a-react-component-library-in-rollup/
+
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
 import dts from 'rollup-plugin-dts';
+import { getFiles } from './scripts/buildUtils';
 
 const packageJson = require('./package.json');
+const extensions = ['.js', '.ts', '.jsx', '.tsx'];
 
 export default [
   {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        // file: packageJson.module,
-        format: 'esm',
-        sourcemap: true,
-
-        dir: 'dist',
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-      },
+    input: [
+      'src/index.ts',
+      ...getFiles('./src/external', extensions),
+      // ...getFiles('./src/hooks', extensions),
+      // ...getFiles('./src/common', extensions),
+      // ...getFiles('./src/utils', extensions),
     ],
+    output: {
+      dir: 'dist',
+      format: 'esm',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      sourcemap: true,
+    },
     plugins: [
       resolve(),
       commonjs(),
       typescript({ 
         tsconfig: './tsconfig.json',
         declaration: true,
-        declarationDir: 'dist',
+        declarationDir: 'dist/types',
       }),
       postcss(),
     ],
   },
   {
-    input: 'dist/esm/types/index.d.ts',
+    input: 'dist/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
     external: [/\.(css|less|scss)$/],
