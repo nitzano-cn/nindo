9aba8a93407fb442a54f1fa0a0ac50d112e58aa4
Daniel Sternlicht <gdblogst@gmail.com>
2022-01-24 12:51:48 +0200

    Adding react-is as extenal

--- packages/nindo/rollup.config.js
+++ packages/nindo/rollup.config.js
@@ -23,22 +23,7 @@ export default [
 		},
 		plugins: [
 			nodeResolve(),
-			commonjs({
-				namedExports: {
-					'node_modules/react/index.js': [
-						'cloneElement',
-						'createContext',
-						'Component',
-						'createElement'
-					],
-					'node_modules/react-dom/index.js': ['render', 'hydrate'],
-					'node_modules/react-is/index.js': [
-						'isElement',
-						'isValidElementType',
-						'ForwardRef'
-					]
-				}
-			}),
+			commonjs(),
 			typescript({
 				tsconfig: './tsconfig.json',
 				declaration: true,
@@ -51,6 +36,7 @@ export default [
 			// To prevent loading react twice
 			'react',
 			'react-dom',
+			'react-is',
 		],
 	},
 	{
